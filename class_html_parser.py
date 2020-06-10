fcb72ee480c18f4bb8a4d66bc39805e49f60e7ae
Izhar Abaasi <izharabbasi103@gmail.com>
2020-06-10 21:32:10 +0500

    add class parser

---
+++ class_html_parser.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+
+
+ITEM_HTML = '''<html><head></head><body>
+<li class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+    <article class="product_pod">
+            <div class="image_container">
+                    <a href="catalogue/a-light-in-the-attic_1000/index.html"><img src="media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg" alt="A Light in the Attic" class="thumbnail"></a>
+            </div>
+                <p class="star-rating Three">
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                </p>
+            <h3><a href="catalogue/a-light-in-the-attic_1000/index.html" title="A Light in the Attic">A Light in the ...</a></h3>
+            <div class="product_price">
+        <p class="price_color">£51.77</p>
+<p class="instock availability">
+    <i class="icon-ok"></i>
+        In stock
+</p>
+    <form>
+        <button type="submit" class="btn btn-primary btn-block" data-loading-text="Adding...">Add to basket</button>
+    </form>
+            </div>
+    </article>
+</li>
+</body></html>
+'''
+
+
+class ParseItem:
+
+    def __init__(self, page):
+        self.soup = BeautifulSoup(ITEM_HTML, "html.parser")
+
+    @property
+    def find_name(self):
+        locators = "article.product_pod h3 a"
+        name = self.soup.select_one(locators).attrs["title"]
+        return name
+
+    @property
+    def find_link(self):
+        locators = "article.product_pod a"
+        link = self.soup.select_one(locators).attrs["href"]
+        return link
+
+    @property
+    def find_price(self):
+        locators = "article.product_pod div.product_price p.price_color"
+        price = self.soup.select_one(locators)
+        return price.string
+
+    @property
+    def find_rating(self):
+        locators = "article.product_pod p.star-rating"
+        rating = self.soup.select_one(locators)
+        classes = rating.attrs['class']
+        rating_classes = [r for r in classes if r != "star-rating"]
+        return rating_classes
+
+
+item = ParseItem(ITEM_HTML)
+print(item.find_name)
+print(item.find_link)
+print(item.find_price)
+print(item.find_rating)
