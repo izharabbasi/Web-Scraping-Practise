77d4343cc9e0402363886682e70bd742cdaa8408
Izhar Abaasi <izharabbasi103@gmail.com>
2020-06-10 15:35:52 +0500

    add 1st example

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+from bs4 import BeautifulSoup
+
+SIMPLE_HTML = '''<html>
+<head></head>
+<body>
+<h1>This is a title</h1>
+<p class="subtitle">Lorem ipsum dolor sit amet. Consectetur edipiscim elit.</p>
+<p>Here's another p without a class</p>
+<ul>
+    <li>Rolf</li>
+    <li>Charlie</li>
+    <li>Jen</li>
+    <li>Jose</li>
+</ul>
+</body>
+</html>'''
+
+simple_soup = BeautifulSoup(SIMPLE_HTML, "html.parser")
+
+print(simple_soup.find("h1").string)
